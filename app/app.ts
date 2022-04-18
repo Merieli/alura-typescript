a8c1e2c3baa9dfd3eed9c15f8197d1da49347de4
Meriéli Manzano <merieli.dev@gmail.com>
2022-04-18 07:28:05 -0300

    Modulo 2 da Aula 04 a 07

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { Negociacao } from "./models/negociacao.js";
+
 // em TS caso seja esquecido algum parametro, será apresentado um erro
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const negociacao = new Negociacao(new Date(), 10, 100); 
+console.log(negociacao.volume)
