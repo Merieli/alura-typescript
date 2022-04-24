ffbf465ea43e823f337dd1307f3f698945227969
Meriéli Manzano <merieli.dev@gmail.com>
2022-04-24 10:14:04 -0300

    Curso part 3: Modulo 2 completo

--- part3-maisTecnicasEBoasPraticas/app/src/views/view.ts
+++ part3-maisTecnicasEBoasPraticas/app/src/views/view.ts
@@ -1,29 +1,24 @@
+import { inspect } from "../decorators/inspect.js";
 import { LogarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
             throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
-        if (escapar) {
-            this.escapar = escapar;
-        }
     }
 
+    //Com decorators a ordem de execução é de cima para baixo, mas ao modificar a classe em que o decorator está aplicado, é aplicada a modificação no método na ordem de baixo para cima
+    @inspect //como inspect não possui a função de recebimento de parametros, ela não precisa ser chamada e os "()" devem ser removidos
     @LogarTempoDeExecucao(true)
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 
